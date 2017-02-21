f5a3dcbd734c1658a011e3728b4c2a9cf546b5cb
csy <csy@localhost>
2017-02-21 20:36:28 +0800

    Initial Commit

---
+++ zhihu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhihu
+//
+//  Created by csy on 17/2/21.
+//  Copyright © 2017年 xyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
